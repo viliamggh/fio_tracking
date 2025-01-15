091acda2da7cdee19bbdb2c679e8d4549475ba79
Viliam Gago <viliam.gago95@gmail.com>
2025-01-15 22:17:41 +0100

    fix providers block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
     }
   }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
 }
 
 data "azurerm_client_config" "current" {}
