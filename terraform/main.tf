c642c8d7122422144b6b4b8a4601f255c12a40b4
Viliam Gago <viliam.gago95@gmail.com>
2025-01-18 08:26:12 +0100

    dbg

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,3 +39,46 @@ resource "azurerm_storage_account" "sa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+# Service Plan
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan
+resource "azurerm_service_plan" "sp" {
+  name                = "splan-${var.service_shortcut}-${var.environment_tag}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "appinsights-${var.service_shortcut}-${var.environment_tag}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  application_type    = "other"
+}
+
+resource "azurerm_linux_function_app" "functions" {
+  name                       = "funcapp-${var.service_shortcut}-${var.environment_tag}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id = azurerm_service_plan.sp.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+  https_only                 = true
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "custom"
+    # SPAUTH_SITEURL           = var.sharepoint_siteurl
+    # SPAUTH_CLIENTID          = var.sharepoint_clientid
+    # SPAUTH_CLIENTSECRET      = var.sharepoint_clientsecret
+
+    FUNCTION_APP_EDIT_MODE   = "readonly"
+    # HASH                     = base64encode(filesha256(var.package))
+    # WEBSITE_RUN_FROM_PACKAGE = "https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${azurerm_storage_container.deployments.name}/${azurerm_storage_blob.appcode.name}${data.azurerm_storage_account_sas.sas.sas}"
+
+    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.appinsights.instrumentation_key
+  }
+
+  site_config {}
+
+}
